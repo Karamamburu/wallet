9b7f47ad2b4e57c6c1cd32a9d6279b0f12ef6596
denis_khmyrov <denis.e.khmyrov@gmail.com>
2023-04-16 11:36:55 +0300

    fix: burger menu set up correctly

--- main/blocks/header/burgerMenu.js
+++ main/blocks/header/burgerMenu.js
@@ -1,8 +1,22 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu  = document.querySelector(".nav-menu");
 const navLinks = document.querySelectorAll(".nav-link");
 
-const setActiveClass = (evt) => {
-  navLinks.forEach((elem) => elem.classList.remove("active"));
-  evt.target.classList.add('active')
+const toggleActiveClass = () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 }
 
-navLinks.forEach((n) => n.addEventListener("click", setActiveClass))
+const removeActiveLinkClass = () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+const setActiveLinkClass = (evt) => {
+  navLinks.forEach((elem) => elem.classList.remove('nav-link__active'));
+  evt.target.classList.add('nav-link__active');
+}
+
+hamburger.addEventListener("click", toggleActiveClass);
+navLinks.forEach((link) => link.addEventListener("click", removeActiveLinkClass));
+navLinks.forEach((link) => link.addEventListener("click", setActiveLinkClass));
